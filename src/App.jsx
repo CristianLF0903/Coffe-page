2f2636fe5733a39debe0a0a88528a4d3e9597433
Cristian Londoño Franco <69688343+CristianLF0903@users.noreply.github.com>
2024-07-31 21:20:40 -0500

    Modificar estructura y definir componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Header from './components/Header';
+import Introduction from './components/Introduction';
+import History from './components/History';
+import Countries from './components/Countries';
+import Characteristics from './components/Characteristics';
+import Benefits from './components/Benefits';
+import Preparation from './components/Preparation';
+import Footer from './components/Footer';
+import './css/App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+      <Introduction />
+      <History />
+      <Countries />
+      <Characteristics />
+      <Benefits />
+      <Preparation />
+      <Footer />
     </>
   )
 }
